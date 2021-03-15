99242d455f51aa14a4266491b4a97d18a16b81a8
kris <kris.kennaway@gmail.com>
2021-03-15 21:41:39 +0000

    Fix warning

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
 setup(
-    ext_modules=cythonize(["dither.pyx"], annotate=True)
+    ext_modules=cythonize(
+        ["dither.pyx"],
+        annotate=True,
+        compiler_directives={'language_level' : "3"}
+    )
 )
